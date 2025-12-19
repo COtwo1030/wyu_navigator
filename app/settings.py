f2c3cfbac0f9a39e0fb8d33bf26f218c0ceb7196
COtwo1030 <2221287593@qq.com>
2025-12-19 18:39:28 +0800

    modify_settings

--- app/settings.py
+++ app/settings.py
@@ -1,12 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+# 加载 .env 文件中的环境变量
+load_dotenv()
+
 # 数据库配置
-ASYNC_DB_URL = "postgresql+asyncpg://postgres:2018@localhost:5432/wyu_navigator"
+ASYNC_DB_URL = os.getenv("ASYNC_DB_URL")
 
 # 邮箱配置
-MAIL_USERNAME = ""
-MAIL_PASSWORD = ""
-MAIL_FROM = "@qq.com"
-MAIL_SERVER = "smtp.qq.com"
-MAIL_PORT = 587
-MAIL_FROM_NAME = "五邑大学校园导航"
-MAIL_STARTTLS = True
-MAIL_SSL_TLS = False
+MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+MAIL_FROM = os.getenv("MAIL_FROM")
+MAIL_SERVER = os.getenv("MAIL_SERVER")
+MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
+MAIL_FROM_NAME = os.getenv("MAIL_FROM_NAME")
+MAIL_STARTTLS = os.getenv("MAIL_STARTTLS", "True").lower() == "true"
+MAIL_SSL_TLS = os.getenv("MAIL_SSL_TLS", "False").lower() == "true"
