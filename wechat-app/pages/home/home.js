fda1ef35f21bea95b777147c9343696a344e5a60
COtwo1030 <2221287593@qq.com>
2026-01-03 01:58:56 +0800

    no

--- wechat-app/pages/home/home.js
+++ wechat-app/pages/home/home.js
@@ -1,5 +1,12 @@
 const config = require('../../config.js')
 Page({
   onShow() {
+    const i = 3
+    const r = Math.floor(Math.random() * 4) + 1
+    wx.setTabBarItem({
+      index: i,
+      iconPath: '/images/tabbar/home.png',
+      selectedIconPath: `/images/tabbar/home-active${r}.png`
+    })
   }
 })
